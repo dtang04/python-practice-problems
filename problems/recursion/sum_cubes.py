84207ef305752ebc782f3711be07b516f59ef095
Dylan Tang <dtang04@linux1.cs.uchicago.edu>
2022-12-28 22:14:23 -0600

    Finished sum_cubes

--- problems/recursion/sum_cubes.py
+++ problems/recursion/sum_cubes.py
@@ -8,8 +8,9 @@ def sum_cubes(n):
     
     This function may not use any loops or list comprehensions.
     """
-
-    pass
+    if n == 0:
+        return 0
+    return n ** 3 + sum_cubes(n-1)
 
 
 #############################################################
