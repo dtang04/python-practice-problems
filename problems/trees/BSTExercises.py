e99c5d89499067961414ed15ae6efb825f8604e6
Dylan Tang <dtang04@linux6.cs.uchicago.edu>
2023-01-12 12:28:52 -0600

    Finished up to add_to_all

--- problems/trees/BSTExercises.py
+++ problems/trees/BSTExercises.py
@@ -1,5 +1,6 @@
+import textwrap
 class Empty:
-
+    
     def __init__(self):
         # nothing to do!
         pass
@@ -21,6 +22,18 @@ class Empty:
 
     def insert(self, n):
         return Node(n, Empty(), Empty())
+    
+    def min_item(self):
+        return
+    
+    def max_item(self):
+        return
+    
+    def balance_factor(self):
+        return 0
+    
+    def balanced_everywhere(self):
+        return True
 
 
 class Node:
@@ -71,11 +84,49 @@ class Node:
             lst += self.right.inorder()
         return lst
 
+    def min_item(self):
+        if self.left.is_empty():
+            return self.value
+        return self.left.min_item()
 
+    def max_item(self):
+        if self.right.is_empty():
+            return self.value
+        return self.right.max_item()
+    
+    def balance_factor(self):
+        left = self.left.height()
+        right = self.right.height()
+        return right - left
+    
+    def balanced_everywhere(self):
+        if abs(self.left.balance_factor()) > 1 or abs(self.right.balance_factor()) > 1 :
+            return False
+        if self.left.balanced_everywhere() == False or self.right.balanced_everywhere() == False:
+            return False
+        return True
+    
+    def add_to_all(self,addval):
+        if self.is_empty():
+            return Empty()
+        current = Node(self.value + addval,None,None)
+        if self.left.is_empty() == False:
+            current.left = self.left.add_to_all(addval)
+        else:
+            current.left = Empty()
+        if self.right.is_empty() == False:
+            current.right = self.right.add_to_all(addval)
+        else:
+            current.right = Empty()
+        return current
 
-if __name__ == "__main__":
-    bst = Empty().insert(42).insert(10).insert(15).insert(63).insert(9).insert(54).insert(99)
-
+if __name__ == "__main__": #BST Tester Function
+    bst = Empty().insert(42).insert(10).insert(15).insert(63).insert(9).insert(54).insert(99).insert(2).insert(121).insert(110).insert(112).insert(115)
+    bst2 = Empty().insert(3).insert(2).insert(4).insert(5)
     print(f"The number of nodes is {bst.num_nodes()}")
     print(f"The height is {bst.height()}")
     print(bst.inorder())
+    print(bst.min_item())
+    print(bst.max_item())
+    bst3 = bst.add_to_all(5)
+    print(bst3.inorder())
