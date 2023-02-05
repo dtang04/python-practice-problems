ca2b9409988df88bde9c7d61ca96e10431529944
Dylan Tang <dtang04@linux6.cs.uchicago.edu>
2023-02-05 12:08:16 -0600

    Finished compute_matching

--- problems/numpy/compute_matching.py
+++ problems/numpy/compute_matching.py
@@ -11,7 +11,8 @@ def compute_matching(x, y):
 
     # YOUR CODE HERE
     # Replace None with an appropriate return value
-    return None
+    arr = x == y 
+    return arr
 
 
 #############################################################
